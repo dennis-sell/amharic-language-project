8d96a10d24a22a4890ae44ab92b9da2351f5d933
feefles <eyeung@seas.upenn.edu>
2015-04-25 14:44:26 -0400

    fixed genesis to align

--- bilingual_data/process.py
+++ bilingual_data/process.py
@@ -1,23 +1,37 @@
 import codecs
 # -*- coding: utf-8 -*-
 
-english = open("eng_genesis.txt")
-amharic = codecs.open("genesis.txt", "r", "utf-8")
+#file1: english, file2: amharic
+def process(file1, file2):
+    english = open(file1 + ".txt")
+    amharic = codecs.open(file2 + ".txt", "r", "utf-8")
+    english_out = open(file1 + "_cleaned.txt", "w")
+    amharic_out = open(file2 + "_cleaned.txt", "w")
 
-english_out = open("eng_genesis_cleaned.txt", "w")
-amharic_out = open("genesis_cleaned.txt", "w")
-for line in english:
-    if ']' in line:
-        i = line.index(']')
-    else: 
-        i = -1
-    # print line[i+1:]
-    english_out.write(line[i+1:])
+    english_chapter_ends = []
+    counter = 0
+    for line in english:
+        if len(line.strip()) == 0:
+            continue
+        if ']' in line:
+            i = line.index(']')
+        else: #hit a chapter end
+            english_chapter_ends.append(counter)
+            i = -1
+        english_out.write(line[i+1:])
+        counter+=1
 
-for line in amharic:
-    if u'\u1364' in line:
-        i = line.index(u'\u1364')
-    else: 
-        i = -1
-    # print line[i+1:]
-    amharic_out.write(line[i+1:].encode('utf-8'))
+    amharic_counter = 0
+    for line in amharic:
+        if u'\u1364' in line:
+            i = line.index(u'\u1364')
+        else: #hit the end of chapter
+            end = english_chapter_ends.pop(0)
+            if amharic_counter <= end-1:
+                amharic_out.write('\n')
+                amharic_counter +=1
+            i = -1
+        amharic_out.write(line[i+1:].encode('utf-8'))
+        amharic_counter +=1
+
+process("eng_genesis", "genesis")

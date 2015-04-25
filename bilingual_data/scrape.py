266a47e2770e938cffeb1c0b99b52259d2feaf94
feefles <eyeung@seas.upenn.edu>
2015-04-25 01:02:51 -0400

    add bilingual data for genesis

---
+++ bilingual_data/scrape.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+
+import requests
+
+#starting url
+url = "https://bible.org/foreign/amharic/gen_toc.htm"
+
+
+r  = requests.get(url)
+data = r.text
+
+soup = BeautifulSoup(data)
+
+out = open("dump.txt", 'w')
+
+links = []
+
+for link in soup.find_all('a'):
+    l = link.get('href')
+    if l != None:
+        links.append(l)
+
+# def clean(text):
+
+
+for link in links:
+    r = requests.get("https://bible.org/foreign/amharic/" + link)
+    data = r.text
+    soup = BeautifulSoup(data)
+    for p in soup.find_all('font'):
+        print p.string.encode('utf-8')
